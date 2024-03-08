9048d87f0104cdaaeac5325650497eeed440dd1b
Max L <maliu2@s.sfusd.edu>
2024-03-08 15:18:08 -0800

    Changes up to Burlingame

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,6 +9,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.PrintCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
@@ -23,13 +24,15 @@ public class RobotContainer {
     private static final Joystick secondary = new Joystick(1);
 
     /* Driver Buttons */
-    private static final JoystickButton zeroGyro = new JoystickButton(driver, 5);
-    // private static final JoystickButton autoSpk = new JoystickButton(driver, 1);
+    private static final JoystickButton zeroGyro = new JoystickButton(driver, 1);
     private static final JoystickButton autoAmp = new JoystickButton(driver, 2);
-    // private static final JoystickButton autoIntake = new JoystickButton(secondary, 2);
+  
     private static final JoystickButton manualShoot = new JoystickButton(secondary, 1);
     private static final JoystickButton manualIntake = new JoystickButton(secondary, 2);
     private static final JoystickButton manualOuttake = new JoystickButton(secondary, 3);
+    private static final JoystickButton regurgitate = new JoystickButton(secondary, 4);
+    private static final JoystickButton autoSpk = new JoystickButton(secondary, 0);
+    private static final JoystickButton autoIntake = new JoystickButton(secondary, 0);
 
     /* Subsystems */
     private final Swerve s_Swerve = new Swerve();
@@ -84,11 +87,27 @@ public class RobotContainer {
         //         new InstantCommand(() -> s_Shooter.setNeck(SpinState.ST), s_Shooter),
         //         s_Intake.new ChangeState(IntakeState.STOW)
         // //     ));
+        // manualShoot.debounce(0.1) // does not check if kinesthetics has note- because this should also work when kinesthetics fails
+        //     .onTrue(s_Shooter.new ChangeNeck(SpinState.ST))
+        //     .whileTrue(s_Shooter.new ChangeState(() -> new Pair<>(
+        //             (1-secondary.getThrottle())/2 * Constants.CommandConstants.speakerShooterAngleMax + Constants.CommandConstants.speakerShooterAngleMin,
+        //             Math.abs(secondary.getY()) * Constants.CommandConstants.shooterSpinMax
+        //     ), true))
+        //     //.whileTrue(new InstantCommand( () -> System.out.println((1-secondary.getThrottle())/2 * Constants.CommandConstants.speakerShooterAngleMax + Constants.CommandConstants.speakerShooterAngleMin)))
+        //     .onFalse(new SequentialCommandGroup(
+        //         new ParallelCommandGroup(
+        //             s_Shooter.new ChangeNeck(kinesthetics, SpinState.FW),
+        //             new WaitCommand(1)
+        //         ),
+        //         s_Shooter.new ChangeNeck(SpinState.ST),
+        //         s_Shooter.new ChangeState(() -> new Pair<>(0d, 0d))
+        //     ));
+        // TEMP SUBWOOFER SCORING
         manualShoot.debounce(0.1) // does not check if kinesthetics has note- because this should also work when kinesthetics fails
             .onTrue(s_Shooter.new ChangeNeck(SpinState.ST))
             .whileTrue(s_Shooter.new ChangeState(() -> new Pair<>(
                     (1-secondary.getThrottle())/2 * Constants.CommandConstants.speakerShooterAngleMax + Constants.CommandConstants.speakerShooterAngleMin,
-                    Math.abs(secondary.getY()) * Constants.CommandConstants.shooterSpinMax
+                    0.60 * Constants.CommandConstants.shooterSpinMax
             ), true))
             .onFalse(new SequentialCommandGroup(
                 new ParallelCommandGroup(
@@ -107,6 +126,9 @@ public class RobotContainer {
         manualOuttake.debounce(0.1)
             .whileTrue(s_Intake.new ChangeState(IntakeState.SPIT))
             .onFalse(s_Intake.new ChangeState(IntakeState.STOW));
+        regurgitate.debounce(.1)
+            .whileTrue(s_Shooter.new ChangeNeck(SpinState.BW))
+            .onFalse(s_Shooter.new ChangeNeck(SpinState.ST));
     }
 
     public Command getAutonomousCommand() {
--- src/main/java/frc/robot/commands/IntakeAuto.java
+++ src/main/java/frc/robot/commands/IntakeAuto.java
@@ -31,7 +31,7 @@ public class IntakeAuto extends SequentialCommandGroup {
             }
         }
         addCommands(
-            sh.toPitch(Constants.Shooter.minimumPitch),
+            // sh.toPitch(Constants.Shooter.minimumPitch),
             sh.new ChangeNeck(SpinState.FW),
             i.new ChangeState(IntakeState.DOWN),
             new WaitUntilCommand(k::shooterHasNote).withTimeout(2),
