44d12b0c7f07c98a104259fc5406f35df5ce899b
Vi-L <viclei1216@gmail.com>
2024-04-04 17:52:12 -0700

    fix merge

--- src/main/java/frc/robot/subsystems/Kinesthetics.java
+++ src/main/java/frc/robot/subsystems/Kinesthetics.java
@@ -19,6 +19,7 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.lib.math.RobotState;
 import frc.robot.Constants;
